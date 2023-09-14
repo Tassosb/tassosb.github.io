7c835340071ce5d6fecf7a14849cbcc96bf0d8de
Tassos Bareiss <tassosbareiss@gmail.com>
2023-09-14 14:12:01 -0600

    Add las cruces vid and more styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        title: ["'Caprasimo', cursive"],
+        title: ["'Ewert', cursive"],
+        // Other fonts I considered...
+        title2: ["'Cabin Sketch', cursive"],
+        title3: ["'Monoton', cursive"],
+        title4: ["'Akronim', cursive"],
+        title5: ["'Bungee Shade', cursive"],
+        title7: ["'Caesar Dressing', cursive"],
+        title7: ["'Cherry Bomb One', cursive"],
+        title8: ["'Faster One', cursive"],
+        title9: ["'Macondo', cursive"],
+        title10: ["'Monoton', cursive"],
+        title11: ["'Mynerve', cursive"],
+        title12: ["'Rock Salt', cursive"],
+        title13: ["'Tilt Prism', cursive"],
+        title14: ["'Train One', cursive"],
+        title15: ["'Rubik Puddles', cursive"],
+        title16: ["'Rock 3D', cursive"],
         western: ["'Smokum', cursive"],
       },
     },
